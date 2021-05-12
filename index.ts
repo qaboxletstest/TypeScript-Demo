34a28f874fa60068809172af439cc53c21d817f4
qaboxletstest <qaboxletstest@gmail.com>
2021-05-12 12:29:20 +0530

    Part 3

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ let personName = 'Avi' // String
 let age = 10 // Number
 let inSports = false // Boolean
 
-// Will cover in much more detail in future videos
+// Will cover each of this topic in much more detail in a separate video in future
 let fruits = ['Apple', 'Mango', 'Banana'] // Array
-let mixArray = [1, true, 'Role'] // Union Type
+let mixArray = [1, true, 'Role'] // Array(Union Type)
 let person = { name: 'Avi', age: 10 } // Object
 let rectArea = (length: number, breadth: number) => length * breadth // Function
 
