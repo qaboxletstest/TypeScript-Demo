57e11b49b9e95121bca1d1ad7892079eb6cc97ee
qaboxletstest <qaboxletstest@gmail.com>
2021-05-17 19:01:38 +0530

    Part-9

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
-// Array
-// let emp = ['John', 1234]
-// emp[0] = 1234
-// emp[1] = 'John'
-// Tuple
-var emp = ['John', 1234];
-console.log(emp);
-emp[0] = 'Max';
-emp[1] = 7890;
-// emp[0] = 1234
-// emp[1] = 'John'
-console.log(emp);
+// Functions
+var greetOne = function () {
+    console.log('Hello');
+};
+// greetOne = 10
+var greetTwo;
+// greetTwo = 10
+greetTwo = function () {
+    console.log('Hello');
+};
+// Parameters - Parameters Data Type, Optional and Default
+// Type
+var multiplication = function (num1, num2) {
+    return num1 * num2;
+};
+// Type - Union Type Parameter
+var id = function (userID) {
+    return userID + " is active";
+};
+// Optional - Should be after all required Parameters - Need to handle Undefined case
+// let sum = (a: number, b: number, c?: number) => {
+//     return a + b + c
+// }
+var sum = function (a, b, c) {
+    if (c) {
+        return a + b + c;
+    }
+    else {
+        return a + b;
+    }
+};
+// console.log(sum(10, 20))
+// console.log(sum(10, 20, 30))
+// Default 
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    return a + b + c;
+};
+// console.log(sum(10, 20))
+var addTwo = function (c, a, b) {
+    if (c === void 0) { c = 10; }
+    return a + b + c;
+};
+console.log(addTwo(undefined, 10, 20));
+// Return Type
+// Type detected by return statement
+var sumOne = function (num1, num2) {
+    return (num1 + num2);
+};
+var sumTwo = function (num1, num2) {
+    return (num1 + num2) + "Test";
+};
+// Explicit Type
+var isBoolean = function (bool1, bool2) {
+    return bool1 && bool2;
+};
+// console.log(isBoolean(true, false))
+// Void Return Type
+var greetThree = function () {
+    console.log('Hello');
+};
--- index.ts
+++ index.ts
@@ -1,17 +1,86 @@
-// Array
-// let emp = ['John', 1234]
-// emp[0] = 1234
-// emp[1] = 'John'
+// Functions
 
-// Tuple
-let emp: [string, number] = ['John', 1234]
+let greetOne = () => {
+    console.log('Hello')
+}
 
-console.log(emp)
+// greetOne = 10
 
-emp[0] = 'Max'
-emp[1] = 7890
 
-// emp[0] = 1234
-// emp[1] = 'John'
+let greetTwo: Function
 
-console.log(emp)
+// greetTwo = 10
+
+greetTwo = () => {
+    console.log('Hello')
+}
+
+// Parameters - Parameters Data Type, Optional and Default
+
+// Type
+let multiplication = (num1: number, num2: number) => {
+    return num1 * num2
+}
+
+// Type - Union Type Parameter
+let id = (userID: number | string) => {
+    return `${userID} is active`
+}
+
+// Optional - Should be after all required Parameters - Need to handle Undefined case
+// let sum = (a: number, b: number, c?: number) => {
+//     return a + b + c
+// }
+
+let sum = (a: number, b: number, c?: number) => {
+    if (c) {
+        return a + b + c
+    } else {
+        return a + b
+    }
+}
+
+// console.log(sum(10, 20))
+// console.log(sum(10, 20, 30))
+
+// Default 
+let add = (a: number, b: number, c: number = 10) => {
+    return a + b + c
+}
+
+// console.log(sum(10, 20))
+
+let addTwo = (c: number = 10, a: number, b: number) => {
+    return a + b + c
+}
+
+console.log(addTwo(undefined, 10, 20))
+
+
+// Return Type
+
+// Type detected by return statement
+let sumOne = (num1: number, num2: number) => {
+    return (num1 + num2)
+}
+
+let sumTwo = (num1: number, num2: number) => {
+    return (num1 + num2) + "Test"
+}
+
+// Explicit Type
+let isBoolean = (bool1: boolean, bool2: boolean): boolean => {
+    return bool1 && bool2
+}
+
+// console.log(isBoolean(true, false))
+
+
+// Void Return Type
+let greetThree = () => {
+    console.log('Hello')
+}
+
+
+
+
