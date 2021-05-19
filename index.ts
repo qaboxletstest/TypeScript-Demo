b0b2ef3d27eb82b2d872a795358f235ecffd980b
qaboxletstest <73457769+qaboxletstest@users.noreply.github.com>
2021-05-19 13:50:57 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,38 @@
 // let sum: Function
 
 // 1. Function Signature
+// let sum : (numOne:number, numTwo:number) => number
 
-let sum: (numOne: number, numTwo: number, numThree?: number) => number
-sum = (a: number, b: number) => {
-    return a + b
-}
+// sum = (a:number, b:number) : number => {
+//     return a + b
+// }
 
-sum = (a: number, b: number, c: number = 10) => {
-    if (c) {
+// Optional Parameter in Function Signature
+
+// let sum : (numOne:number, numTwo:number, numThree?:number) => number
+
+// sum = (a:number, b:number, c?:number) : number => {
+//     if(c){
+//         return a + b + c
+//     } else {
+//         return a + b
+//     }
+    
+// }
+
+// Default Parameter in Function Signature
+
+let sum : (numOne:number, numTwo:number, numThree?:number) => number
+
+sum = (a:number, b:number, c:number = 10) : number => {
+    if(c){
         return a + b + c
     } else {
         return a + b
     }
+    
 }
+
 
 let add = (a: number, b: number, c: number = 10): number => {
     return a + b + c
@@ -21,14 +40,13 @@ let add = (a: number, b: number, c: number = 10): number => {
 
 // 2. Function Type
 
-type addition = (numOne: number, numTwo: number, numThree?: number) => number
+type addition = (numOne:number, numTwo:number, numThree?:number) => number
 
-let aggregate: addition = (a: number, b: number, c?: number) => {
-    if (c) {
+let aggregate : addition = (a:number, b:number, c:number = 10) : number => {
+    if(c){
         return a + b + c
     } else {
         return a + b
     }
+    
 }
-
-
