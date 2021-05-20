9a1307708de1b676c2b0c531ba328c48ddbf8635
qaboxletstest <73457769+qaboxletstest@users.noreply.github.com>
2021-05-20 19:09:07 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,22 +2,25 @@
 
 // ARRAYS - 
 
-let numArray1: number[]
+let numArray1: number[] // OR
 let numArray2: Array<number>
 
+// ARRAY TYPES
+
 type numArray = Array<number>
-type strArray = Array<string>
+type strArray = Array<boolean>
 
 // FUNCTIONS - SINGLE GENERIC TYPE
 
-let lastItem = (arr: Array<number>) => {
-    return arr[arr.length - 1]
-}
+// let lastItem = (arr: Array<any>) => {
+//     return arr[arr.length - 1]
+// }
 
 // const item1 = lastItem([1, 2, 3])
 // const item2 = lastItem(['a', 'b', 'c'])
 
 
+// lastItem Function With Generics
 // let lastItem = <T>(arr: Array<T>): T => {
 //     return arr[arr.length - 1]
 // }
@@ -25,43 +28,57 @@ let lastItem = (arr: Array<number>) => {
 // const item1 = lastItem([1, 2, 3])
 // const item2 = lastItem<string>(['a', 'b', 'c'])
 
+
 // FUNCTIONS - MULTIPLE GENERIC TYPE
 
 // function getEmployee(id: number, code: string): string {
 //     return `${id} - ${code}`
 // }
 
-function getEmployee<X, Y = string>(id: X, code: Y): string {
+// getEmployee(1, "CD01")
+// getEmployee("CD01", "CD01")
+
+// getEmployee Function With Generics
+function getEmployee<X, Y= string>(id: X, code: Y): string {
     return `${id} - ${code}`
 }
 
-getEmployee<number, number>(1, 7); // number, number
-getEmployee<number, string>(1, "Steve SMith"); // number, string
-getEmployee<string, string>('JD01', "Jack Daniel"); // string, string
-
+getEmployee(1, "CD01") // number, string
+getEmployee<string>("CD01", "CD01") // string, string
 
 // FUNCTIONS WITH OBJECTS
-// const addFullNameProp = (obj: { firstname: string, lastname:string}) => {
+// const addFullNameProp = (obj : {firstname:string, lastname:string}) => { // In tsconfig.json comment out strict option
 //     return {
 //         ...obj,
 //         FullName: `${obj.firstname} ${obj.lastname}`
 //     }
 // }
 
-const addFullNameProp = <T extends { firstname: string, lastname: string }>(obj: T) => {
+// addFullNameProp Function With Generics
+
+const addFullNameProp = <T extends {firstname:string, lastname:string}>(obj : T) => { // In tsconfig.json comment out strict option
     return {
         ...obj,
         FullName: `${obj.firstname} ${obj.lastname}`
     }
 }
 
-const perOne = addFullNameProp({ firstname: 'John', lastname: "Smith", age: 32 })
+
+const perOne = addFullNameProp({ firstname: 'John', lastname: "Smith", age:35})
 
 console.log(perOne)
 
 
 
 // INTERFACES
+
+// interface IPerson {
+//     name: string,
+//     age: number,
+//     misc: string
+// }
+
+// IPerson Interface With Generics
 
 interface IPerson<T> {
     name: string,
@@ -75,7 +92,7 @@ let p1: IPerson<string> = {
     misc: 'Some secret'
 }
 
-let p2: IPerson<string[]> = {
+let p2: IPerson<string []> = {
     name: "Steve",
     age: 45,
     misc: ['Some', 'secret']
