9035a7c02c4c59875925ef2dcee20eae1e183f81
qaboxletstest <73457769+qaboxletstest@users.noreply.github.com>
2021-05-22 14:24:10 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// GENERICS
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
