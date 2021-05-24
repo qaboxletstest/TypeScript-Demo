5fb74f04ea13bac10e1984d84da89cb9e10c3c12
qaboxletstest <qaboxletstest@gmail.com>
2021-05-24 18:24:37 +0530

    Part-16

--- public/index.js
+++ public/index.js
@@ -1,26 +1,73 @@
 "use strict";
-// const h1 = document.querySelector("h1")!
-// const h1 = document.querySelector("h1")
-// if (h1) {
-//     console.log(h1.textContent)
+// Class
+class ITDept {
+    // Constructor
+    constructor(name, age, isHead, email) {
+        this.name = name;
+        this.age = age;
+        this.isHead = isHead;
+        this.email = email;
+    }
+    // Methods
+    summary() {
+        if (this.isHead) {
+            return `${this.name} leads IT department and is ${this.age} years old. (Email - ${this.email})`;
+        }
+        else {
+            return `${this.name} works into IT department and is ${this.age} years old. (Email - ${this.email})`;
+        }
+    }
+}
+// Instantiation
+const it1 = new ITDept('John Doe', 32, true, 'john.doe@abc.xom');
+const it2 = new ITDept('Sam Doe', 35, false, 'sam.doe@abc.xom');
+console.log(it1, it2);
+// Access Properties and Invoke Methods
+console.log(it1.name);
+console.log(it1.summary());
+// Custom Type
+const names = [];
+names.push(it1);
+names.push(it2);
+// Change Properties outside the class
+it1.name = "Steve Smith";
+it2.age = 40;
+console.log(it1, it2);
+console.log(names);
+// Inheritance - Single and Multiple
+class A {
+    constructor(a) {
+        this.a = a;
+    }
+}
+class B extends A {
+    constructor(a, b) {
+        super(a);
+        this.b = b;
+    }
+}
+const b1 = new B('a', 'b');
+// Access Parent/Self Properties
+console.log(b1.a);
+console.log(b1.b);
+console.log(b1);
+// Multi-Level - Allowed
+class C extends B {
+}
+// Multiple - Not Allowed
+// class D extends A, B {
 // }
-// console.log(h1!.textContent)
-// console.log(h1?.textContent)
-// const form = document.querySelector(".new-emp-form")!
-// const form = document.querySelector<HTMLFormElement>(".new-emp-form")!
-var form = document.querySelector(".new-emp-form");
-// console.log(form.children)
-var departmentElm = document.querySelector('#department');
-var nameElm = document.querySelector('#name');
-var ageElm = document.querySelector('#age');
-var isHeadElm = document.querySelector('#departmentHead');
-var emailElm = document.querySelector('#email');
-var clear = document.querySelector('#clear');
-form.addEventListener("submit", function (e) {
+const form = document.querySelector(".new-emp-form");
+const departmentElm = document.querySelector('#department');
+const nameElm = document.querySelector('#name');
+const ageElm = document.querySelector('#age');
+const isHeadElm = document.querySelector('#departmentHead');
+const emailElm = document.querySelector('#email');
+const clear = document.querySelector('#clear');
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(departmentElm.value, nameElm.value, ageElm.valueAsNumber, isHeadElm.value, emailElm.value);
+});
+clear.addEventListener("click", () => {
     form.reset();
 });
-// clear.addEventListener("click", () => {
-//     form.reset()
-// })
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,94 @@
-// const h1 = document.querySelector("h1")!
-// const h1 = document.querySelector("h1")
 
-// if (h1) {
-//     console.log(h1.textContent)
+// Class
+
+class ITDept {
+    // Properties 
+    name: string
+    age: number
+    isHead: boolean
+    email: string
+
+    // Constructor
+    constructor(name: string, age: number, isHead: boolean, email: string) {
+        this.name = name
+        this.age = age
+        this.isHead = isHead
+        this.email = email
+    }
+
+    // Methods
+    summary(): string {
+        if (this.isHead) {
+            return `${this.name} leads IT department and is ${this.age} years old. (Email - ${this.email})`
+        } else {
+            return `${this.name} works into IT department and is ${this.age} years old. (Email - ${this.email})`
+        }
+    }
+}
+
+// Instantiation
+
+const it1 = new ITDept('John Doe', 32, true, 'john.doe@abc.xom')
+const it2 = new ITDept('Sam Doe', 35, false, 'sam.doe@abc.xom')
+
+console.log(it1, it2)
+
+// Access Properties and Invoke Methods
+console.log(it1.name)
+console.log(it1.summary())
+
+// Custom Type
+
+const names: ITDept[] = []
+names.push(it1)
+names.push(it2)
+
+// Change Properties outside the class
+
+it1.name = "Steve Smith"
+it2.age = 40
+
+console.log(it1, it2)
+
+console.log(names)
+
+// Inheritance - Single and Multiple
+
+class A {
+    a: string
+    constructor(a: string) {
+        this.a = a
+    }
+}
+
+class B extends A {
+    b: string
+    constructor(a: string, b: string) {
+        super(a)
+        this.b = b
+    }
+}
+
+const b1 = new B('a', 'b')
+
+// Access Parent/Self Properties
+console.log(b1.a)
+console.log(b1.b)
+console.log(b1)
+
+// Multi-Level - Allowed
+class C extends B {
+
+}
+
+// Multiple - Not Allowed
+// class D extends A, B {
+
 // }
 
-// console.log(h1!.textContent)
 
-// console.log(h1?.textContent)
 
-// const form = document.querySelector(".new-emp-form")!
-// const form = document.querySelector<HTMLFormElement>(".new-emp-form")!
 const form = document.querySelector(".new-emp-form") as HTMLFormElement
-// console.log(form.children)
 const departmentElm = document.querySelector('#department') as HTMLSelectElement
 const nameElm = document.querySelector('#name') as HTMLInputElement
 const ageElm = document.querySelector('#age') as HTMLInputElement
@@ -29,10 +105,9 @@ form.addEventListener("submit", (e: Event) => {
         isHeadElm.value,
         emailElm.value
     )
-    form.reset()
 })
 
-// clear.addEventListener("click", () => {
-//     form.reset()
-// })
+clear.addEventListener("click", () => {
+    form.reset()
+})
 
